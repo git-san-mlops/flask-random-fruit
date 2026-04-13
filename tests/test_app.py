764e684d44aadfae549ef1b5a46948399d19bdab
Sanjana <sanjana.sinha@outlook.com>
2026-04-13 16:10:45 +0530

    modify test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,16 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from app import app
+
 
 def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
+
 
 def test_random_fruit():
     client = app.test_client()
